b563539bd3381df36949acb4ba51b08a0931b089
rfonseca985 <rfonseca985@gmail.com>
2021-09-22 12:44:40 -0300

    Enviando imagem via endpoint

--- src/main/java/cursomodelagemconceitual/CursomcApplication.java
+++ src/main/java/cursomodelagemconceitual/CursomcApplication.java
@@ -1,17 +1,13 @@
 package cursomodelagemconceitual;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import cursomodelagemconceitual.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+
 	
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -19,6 +15,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {	
-		s3Service.uploadFile("C:\\Users\\rfons\\OneDrive\\Área de Trabalho\\Udemy\\ws-sts\\spring-boot-ionic-backend-master\\teste.jpg");
-	}	
+		
+	}
 }
+
