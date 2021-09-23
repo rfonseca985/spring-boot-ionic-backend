981e9cf1c452d59b54810069e89d4c674443c3d4
rfonseca985 <rfonseca985@gmail.com>
2021-09-23 12:09:19 -0300

    Endpoints para buscar estados e cidades

--- src/main/java/cursomodelagemconceitual/config/SecurityConfig.java
+++ src/main/java/cursomodelagemconceitual/config/SecurityConfig.java
@@ -39,13 +39,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**", };
 
-	private static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**",
+	private static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**",
+			"/categorias/**",
+			"/estados/**"
 
 	};
 
 	private static final String[] PUBLIC_MATCHERS_POST = { 
 			"/clientes/",
-			"/auth/forgot/**"
+			"/auth/forgot/**",
+			
 	};
 	
 	@Override
---
+++ src/main/java/cursomodelagemconceitual/services/EstadoService.java
@@ -0,0 +1,19 @@
+package cursomodelagemconceitual.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cursomodelagemconceitual.domain.Estado;
+import cursomodelagemconceitual.repositories.EstadoRepository;
+
+@Service
+public class EstadoService {
+	@Autowired
+	private EstadoRepository repo;
+
+	public List<Estado> findAll() {
+		return repo.findAllByOrderByNome();
+	}
+}
